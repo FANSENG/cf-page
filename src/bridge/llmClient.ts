0aae403944347feb3a7dfda8bc1a1efe2d0fc41a
fanxinhao <fanxinhao@bytedance.com>
2024-10-28 14:25:19 +0800

    feat: core code to the cloud

--- src/bridge/llmClient.ts
+++
@@ -1,8 +0,0 @@
-import OpenAI from 'openai';
-import { GLM4FlashConfig } from '../config/config';
-
-export const LLMClient = new OpenAI({
-    baseURL: GLM4FlashConfig.BaseURI,
-    apiKey: GLM4FlashConfig.APIKEY,
-    dangerouslyAllowBrowser:true,
-});
